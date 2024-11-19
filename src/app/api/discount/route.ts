b35aa8a50de919d599aa7519b58bd358459f64bf
Ulemj Bayan-Undur <ue@Ue-MacBook-Pro.local>
2024-11-19 12:44:49 +0800

    discount iin create , get

---
+++ src/app/api/discount/route.ts
@@ -0,0 +1,29 @@
+import { DiscountModel } from "@/app/model/discount";
+import { NextRequest } from "next/server";
+
+export const GET = async () => {
+  try {
+    const discounts = await DiscountModel.find();
+
+    return Response.json({ discounts });
+  } catch (error) {
+    return Response.json({ message: error });
+  }
+};
+
+export const POST = async (request: NextRequest) => {
+  const { name, discount_percent, time_used, max_used } = await request.json();
+
+  try {
+    const discount = await DiscountModel.create({
+      name,
+      discount_percent,
+      time_used,
+      max_used,
+    });
+
+    return Response.json({ message: "success", discount });
+  } catch (error) {
+    return Response.json({ message: error });
+  }
+};
