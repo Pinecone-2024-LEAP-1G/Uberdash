7d9a65f16f04de2621efc9a17922613684242161
24LP6830 24LP6830 <24LP6830@admins-iMac.local>
2024-11-19 09:56:54 +0800

    shadcn and mongodb setup

---
+++ src/lib/db/index.ts
@@ -0,0 +1,22 @@
+import mongoose, { Connection } from "mongoose";
+
+// Declaring a variable to store the cached database connection
+let cachedConnection: Connection | null = null;
+
+// Function to establish a connection to MongoDB
+export async function connectToMongoDB() {
+  // If a cached connection exists, return it
+  if (cachedConnection) {
+    console.log("Using cached db connection");
+    return cachedConnection;
+  }
+  try {
+    const cnx = await mongoose.connect(process.env.MONGODB_URI!);
+    cachedConnection = cnx.connection;
+    console.log("New mongodb connection established");
+    return cachedConnection;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
