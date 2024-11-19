cb69776310fe1317672c0e5cb9eabf268883167c
Angarag <anguulziibadrah@gmail.com>
2024-11-19 12:33:35 +0800

    user modeliin password hesgiig hijj duusgaw

--- src/lib/models/users.model.ts
+++ src/lib/models/users.model.ts
@@ -1,12 +1,14 @@
 import { Model, Schema, model, models } from "mongoose";
 
 type User = {
-  _id: string;
+  _id: Schema.Types.ObjectId;
   email: string;
+  password: string;
 };
 
 const userSchema = new Schema<User>({
   email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 export const UserModel: Model<User> =
