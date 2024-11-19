0804a37e91510df21086de1d8debe2770a5e83c0
24LP8972 24LP8972 <24LP8972@admins-iMac.local>
2024-11-19 12:03:01 +0800

    zassan

--- src/lib/models/category.ts
+++ src/lib/models/category.ts
@@ -1,20 +1,13 @@
-import { Model, Schema, model, models } from "mongoose"
+import { Model, Schema, model, models } from "mongoose";
 
 type Category = {
-  
-    _id: Schema.Types.ObjectId;
+  _id: Schema.Types.ObjectId;
   name: string;
-
 };
 
-const CategorySchema = new Schema<Category>(
-  {
-    name: { type: String, required: true },
-   
-   
-  },
-  
-);
+const CategorySchema = new Schema<Category>({
+  name: { type: String, required: true },
+});
 
 export const CategoryModel: Model<Category> =
   models.Category || model<Category>("Category", CategorySchema);
