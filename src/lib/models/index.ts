4d5f6332ab7fb6ac43fcadf07ec567622dbab01f
24LP6830 24LP6830 <24LP6830@admins-iMac.local>
2024-11-20 10:23:04 +0800

    model uudaa zassan+ order-item hiisen

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -2,3 +2,11 @@ export * from "./address.model";
 export * from "./menu-item.model";
 export * from "./category";
 export * from "./users.model";
+export * from "./discount";
+export * from "./order";
+export * from "./order-item.model"
+export * from "./restaurant-branch.model";
+export * from "./restaurant-owner.model";
+export * from "./restaurant.model";
+export * from "./review.model";
+export * from "./users.model"
--- src/lib/models/order.ts
+++ src/lib/models/order.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {models,  Schema } from "mongoose";
 
 export type Order = {
   id: Schema.Types.ObjectId;
@@ -72,5 +72,5 @@ const OrderSchema = new Schema<Order>({
   },
 });
 
-const OrderModel = mongoose.model<Order>("order", OrderSchema);
+const OrderModel = mongoose.model<Order>("orders", OrderSchema);
 export default OrderModel;
