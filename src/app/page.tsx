d3c0d2a8dc5493aec0b4fbde1aee1518ffd0f202
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-11-20 11:13:31 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-// import { Sidebar } from "lucide-react";
-import { MenuItem } from "../components/MenuItem";
-import { RestrauntMenu } from "../components/RestrauntMenu";
+import { Header } from "@/components/layout/header";
+import { Sidebar } from "lucide-react";
 
 const Home = () => {
   return (
     <div>
-      <MenuItem
-        name=" Pappa Johns Burger"
-        image="https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvM2MzOTliMTJkYTliZDIzMjUyZmVhZjAyODk2MTBjYmMvNzgzNTQyOGIyODZhY2I1NzY0NmEyNTZjODk3YzBlOWUuanBlZw=="
-        duration=" 25-40 min"
-        points="4.3"
-        bonus="Buy 1,Get 1 Free"
-      />
-
-      <RestrauntMenu />
+      <Header />
     </div>
   );
-
-  //  <Sidebar />;
 };
 export default Home;
