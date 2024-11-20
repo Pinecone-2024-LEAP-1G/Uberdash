2b3b0e2c3291c5f8ebdb7aa8e169d5da25ec0948
24LP6377 24LP6377 <24LP6377@admins-iMac.local>
2024-11-20 11:44:12 +0800

    createUser bolon getUser hiij duusgaw

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,30 @@
+import { UserModel } from "@/lib/models";
+import { NextRequest } from "next/server";
+
+
+export const GET = async () => {
+  try {
+    const users = await UserModel.find();
+
+    return Response.json({ users });
+  } catch (error) {
+    return Response.json({ message: error });
+  }
+};
+
+console.log("ss")
+export const POST = async (request: NextRequest) => {
+  const {  email, password } = await request.json();
+
+  try {
+    const user = await UserModel.create({
+      
+      email,
+      password,
+    });
+
+    return Response.json({ message: "success", user });
+  } catch (error) {
+    return Response.json({ message: error });
+  }
+};
