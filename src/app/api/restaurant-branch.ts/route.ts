d7d00c9f74c2dd7f0da43d724aef16bfdd703347
24LP6830 24LP6830 <24LP6830@admins-iMac.local>
2024-11-20 12:30:15 +0800

    za yg bolson

--- src/app/api/restaurant-branch.ts/route.ts
+++ src/app/api/restaurant-branch.ts/route.ts
@@ -1,27 +1,24 @@
 import { RestaurantBranchModel } from "@/lib/models/restaurant-branch.model";
 import { NextRequest } from "next/server";
 
-export const GET = async() =>{
-    try{
-        const restaurantBranches = RestaurantBranchModel.find();
-        return Response.json({branches : restaurantBranches})
-    }catch(error)
-    {
-        return Response.json({error})
-    }
-}
+export const GET = async () => {
+  try {
+    const restaurantBranches = RestaurantBranchModel.find();
+    return Response.json({ branches: restaurantBranches });
+  } catch (error) {
+    return Response.json({ error });
+  }
+};
 
-
-export const POST = async(req : NextRequest) =>{
-    console.log("hehe")
-    const {restaurantId, branchName, location} = await req.json();
-    console.log(req.json())
-    try{
-        const newRestaurantBranch = RestaurantBranchModel.create({restaurantId, branchName, location},{new : true});
-        return Response.json({branch : newRestaurantBranch})
-    }
-    catch(error)
-    {
-        return Response.json({error})
-    }
-}
+export const POST = async (req: NextRequest) => {
+  const { restaurantId, branchName, location } = await req.json();
+  try {
+    const newRestaurantBranch = RestaurantBranchModel.create(
+      { restaurantId, branchName, location },
+      { new: true }
+    );
+    return Response.json({ branch: newRestaurantBranch });
+  } catch (error) {
+    return Response.json({ error });
+  }
+};
