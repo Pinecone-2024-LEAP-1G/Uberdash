6862c46c04e49951cf892bd353d43ad6e1246ce8
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-11-20 12:32:29 +0800

    update

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,16 @@
-import { NavigationMenu } from "@radix-ui/react-navigation-menu";
+import { Search } from "lucide-react";
+import { Input } from "../ui/input";
 
 export const Header = () => {
   return (
-    <div className="container mx-auto">
-      <NavigationMenu className="w-full border-2 border-black border-solid h-fit "></NavigationMenu>
+    <div className="border rounded-2xl flex w-full justify-center">
+      <div className="flex justify-around">
+        <div className="flex items-center w-48 ">Delivery Boys</div>
+        <div className="bg-[#F3F3F3] rounded-full flex items-center justify-center gap-6 pl-6 pr-12 w-full">
+          <Search />
+          <Input className="bg-[#F3F3F3] border-none" placeholder="search" />
+        </div>
+      </div>
     </div>
   );
 };
