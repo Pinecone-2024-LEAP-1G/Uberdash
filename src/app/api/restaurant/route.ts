fd0393927800fa7cfcf992326b4f59a1a469d9c2
24LP6830 24LP6830 <24LP6830@admins-iMac.local>
2024-11-20 12:31:02 +0800

    za bolson2

--- src/app/api/restaurant/route.ts
+++ src/app/api/restaurant/route.ts
@@ -1,24 +1,26 @@
 import { NextRequest } from "next/server";
 import { RestaurantModel } from "@/lib/models";
 
-export const GET = async() =>{
-    try{
-        const restaurants = RestaurantModel.find();
-        return Response.json({restaurants})
-    }catch(error)
-    {
-        return Response.json({error})
-    }
-}
+export const GET = async () => {
+  try {
+    const restaurants = RestaurantModel.find();
+    return Response.json({ restaurants });
+  } catch (error) {
+    return Response.json({ error });
+  }
+};
 
-
-export const POST = async(req : NextRequest) =>{
-    const {name , image, banner, info} =await req.json();
-    try{
-        const newRestaurant =await RestaurantModel.create({name, image, banner,info})
-        return Response.json({newRestaurant})
-    }catch(error)
-    {
-        return Response.json({error})
-    }
-}
+export const POST = async (req: NextRequest) => {
+  const { name, image, banner, info } = await req.json();
+  try {
+    const newRestaurant = await RestaurantModel.create({
+      name,
+      image,
+      banner,
+      info,
+    });
+    return Response.json({ newRestaurant });
+  } catch (error) {
+    return Response.json({ error });
+  }
+};
