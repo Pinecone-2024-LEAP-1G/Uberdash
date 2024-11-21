6209ff0e1879d54c8eca7a4fe57918acd311eab9
24LP8972 24LP8972 <24LP8972@admins-iMac.local>
2024-11-21 14:23:26 +0800

    sign-in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { Drawers } from "@/components/Drawer";
 import { MenuItem } from "../components/MenuItem";
 import { RestrauntMenu } from "../components/RestrauntMenu";
 import { MenuItemLastCard } from "../components/FoodCardPlus";
-import { Singup } from "@/components/Sign-in";
+
 
 const Home = () => {
   return (
-    <div>
+    <div className="px-11">
       <MenuItem
         name=" Pappa Johns Burger"
         image="https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvM2MzOTliMTJkYTliZDIzMjUyZmVhZjAyODk2MTBjYmMvNzgzNTQyOGIyODZhY2I1NzY0NmEyNTZjODk3YzBlOWUuanBlZw=="
@@ -39,7 +39,7 @@ const Home = () => {
         like="(500)"
       />
       <Footer />
-      <Singup />
+
     </div >
   );
 };
--- src/components/Sign-in.tsx
+++ src/components/Sign-in.tsx
@@ -2,7 +2,7 @@ import { Input } from "@/components/ui/input"
 
 export const Singup = () => {
     return (
-        <div className="w-[332px] h-[536px]  justify-center flex p-2 ">
+        <div className="w-[332px] h-[536px]  justify-center flex  ">
             <div className="p-7" >
                 <div className="text-2xl">
                     <p>What's your phone number or email?</p>
@@ -11,30 +11,25 @@ export const Singup = () => {
                     <div className="py-4">
                         <Input className="w-full h-[48px] bg-slate-200   " type="email" placeholder="Enter phone number or email" />
                     </div>
-
                     <button className="w-full h-[48px] bg-black flex justify-center text-center py-3 text-white rounded-xl ">Continue</button>
                 </div>
-
                 <div className="flex items-center gap-3">
                     <div className="border-[1px] w-[149px] border-[#D6D8DB] "></div>
                     <p>or</p>
                     <div className="border-[1px] w-[149px] border-[#D6D8DB] "></div>
                 </div>
-
                 <div  >
-
                     <button className="w-full h-[48px] bg-slate-200 flex justify-center text-center py-3 text-black rounded-xl mt-6 " >
                         <img src="./google.svg" className="mr-2 py-1" />
                         Continue with Google</button>
                 </div>
-
                 <div className="flex items-center gap-3 py-8">
                     <div className="border-[1px] w-[149px] border-[#D6D8DB] "></div>
                     <p>or</p>
                     <div className="border-[1px] w-[149px] border-[#D6D8DB] "></div>
                 </div>
 
-                <div className="mt-8">
+                <div className="mt-8 pb-10">
                     <p>By proceeding, you consent to get calls, WhatsApp or SMS/RCS messages, including by automated means, from Uber and its affiliates to the number provided.</p>
                 </div>
             </div>
