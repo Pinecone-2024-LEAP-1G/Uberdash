068334f9fa5f6bf9039d0f4ec5b2ce77beceebb6
24LP8972 24LP8972 <24LP8972@admins-iMac.local>
2024-11-21 12:00:56 +0800

    Drawer button

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,17 +1,43 @@
-export const Drawer = () => {
+"use client"
+import { Button } from "@/components/ui/button"
+import { Dialog } from '@radix-ui/react-dialog'
+import {
+    Sheet,
+    SheetContent,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
+const SHEET_SIDES = ["left"] as const
+
+type SheetSide = (typeof SHEET_SIDES)[number]
+
+export function Drawers() {
     return (
-        <div className="w-[252px] h-[1100px] border">
-            <div className="pt-6 ">
-                <button className="w-[252px] h-[56px] bg-black rounded-2xl flex text-center justify-center text-white py-4 ">Sign up</button>
-                <button className="w-[252px] h-[56px] bg-slate-300 rounded-2xl flex text-center justify-center text-black py-4 mt-2 ">Log in</button>
-            </div>
+        <Dialog>
+            {SHEET_SIDES.map((side) => (
+                <Sheet key={side}>
+                    <SheetTitle></SheetTitle>
+                    <SheetTrigger asChild>
+                        <Button variant="outline">{side}</Button>
+                    </SheetTrigger>
+                    <SheetContent side={side} style={{ maxWidth: '335px' }}>
 
+                        <div className="grid gap-4 py-4">
+                            <button className="h-[46px] bg-black rounded-2xl text-base flex text-center justify-center text-white py-3 ">Sign up</button>
+                            <button className="h-[46px] bg-slate-300 rounded-2xl flex text-center justify-center text-black py-3  ">Log in</button>
+                        </div>
+                        <div>
+                            <button className="flex pt-3">Create a business account</button>
+                            <button className="flex pt-4">Add your restaurant</button>
+                            <button className="flex pt-4">Sign up to deliver</button>
+                        </div>
 
-            <div className=" pt-6">
-                <button className="flex pt-3">Create a business account</button>
-                <button className="flex pt-4">Add your restaurant</button>
-                <button className="flex pt-4">Sign up to deliver</button>
-            </div>
-        </div>
+                    </SheetContent>
+                </Sheet>
+            ))}
+        </Dialog>
     )
 }
+
+
