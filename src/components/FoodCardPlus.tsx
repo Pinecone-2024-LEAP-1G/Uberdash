98553759752458963c5054b6ca8e5c496da7f65b
24LP4300 24LP4300 <24LP4300@admins-iMac.local>
2024-11-20 19:56:28 -0800

    food-card-with-plus-sign-hiiv

---
+++ src/components/FoodCardPlus.tsx
@@ -0,0 +1,55 @@
+import PlusSign from "./ui/PlusSignSvg";
+import { LikeSvg } from "./ui/Like-svg";
+
+type food = {
+  image: string;
+  mostLiked: string;
+  name: string;
+  price: string;
+  percentage: string;
+  like: string;
+};
+
+export const FoodCard = ({
+  image,
+  mostLiked,
+  name,
+  price,
+  percentage,
+  like,
+}: food) => {
+  return (
+    <div className="w-[220px] h-[252px] mt-4 ml-4   ">
+      <div
+        className="w-[220px] h-[188px]  relative cursor-pointer rounded-xl"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div
+          className="absolute inset-0 bg-slate-400 rounded-xl"
+          style={{ opacity: 0.2 }}
+        ></div>
+        <div className="w-[98px] h-[24px] bg-[#0e8345] text-[12px] font-medium px-3 py-1 absolute top-2  rounded-br-[12px] rounded-tr-[12px]">
+          <p className="text-white">{mostLiked}</p>
+        </div>
+        <div className="w-9 h-9 rounded-full bg-white flex justify-center items-center  absolute bottom-2 right-2 hover:bg-slate-200">
+          <PlusSign />
+        </div>
+      </div>
+      <div>
+        <p>{name}</p>
+        <div className="flex items-center gap-1 text-[14px] font-normal ">
+          <p>{price}</p>
+          <div className="w-[3px] h-[3px] bg-[black] rounded-full"></div>
+          <LikeSvg />
+          <p>{percentage}</p>
+          <p>{like}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/RestrauntMenu.tsx
+++ src/components/RestrauntMenu.tsx
@@ -19,7 +19,7 @@ export const RestrauntMenu = ({
   image,
 }: Restraunt) => {
   return (
-    <div className="max-w-xl h-[147px] rounded-xl flex cursor-pointer justify-between border">
+    <div className="max-w-xl h-[147px] rounded-xl flex cursor-pointer justify-between border ">
       <div className="w-[408px] flex py-4 pl-4 flex-col ">
         <p className="text-[16px] font-medium">{name}</p>
         <div className="flex items-center gap-1 text-[14px] font-normal mt-[4px]">
@@ -44,6 +44,10 @@ export const RestrauntMenu = ({
           backgroundSize: "cover",
         }}
       >
+        <div
+          className="absolute inset-0 bg-slate-400 rounded-tr-[12px] rounded-br-[12px]"
+          style={{ opacity: 0.2 }}
+        ></div>
         <div className="w-9 h-9 rounded-full bg-white flex justify-center items-center  absolute bottom-2 right-2 hover:bg-slate-200">
           <PlusSign />
         </div>
