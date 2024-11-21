0050097ec12fcfda7907371f9faf6fd61a265fe9
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-11-21 10:39:32 +0800

    checkout hiilee

--- src/components/utils/FilteredOptions.tsx
+++ src/components/utils/FilteredOptions.tsx
@@ -7,7 +7,6 @@ export const filterOptions = [
   { id: 6, label: "Price", value: "price" },
   { id: 7, label: "Dietary", value: "dietary" },
   { id: 8, label: "Sort", value: "sort" },
-  { id: 9, label: "SNAP", value: "snap" },
 ];
 export const mockItems = [
   { id: 1, name: "Item 1", category: "uberOne", rating: 4.5 },
