52c9a46102e8b5eab3166ba7820a398accbf7495
24LP8972 24LP8972 <24LP8972@admins-iMac.local>
2024-11-21 10:33:39 +0800

    Drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,17 +1,16 @@
 export const Drawer = () => {
     return (
-        <div>
-            <div className="w-[252px] h-[56px] bg-black rounded-2xl flex text-center justify-center text-white ">
-                <button>Sign up</button>
+        <div className="w-[252px] h-[1100px] border">
+            <div className="pt-6 ">
+                <button className="w-[252px] h-[56px] bg-black rounded-2xl flex text-center justify-center text-white py-4 ">Sign up</button>
+                <button className="w-[252px] h-[56px] bg-slate-300 rounded-2xl flex text-center justify-center text-black py-4 mt-2 ">Log in</button>
             </div>
 
-            <div className="w-[252px] h-[56px] bg-slate-300 rounded-2xl flex text-center justify-center text-black  ">
-                <button>Log in</button>
-            </div>
-            <div className="">
-                <button className="flex">Create a business account</button>
-                <button className="flex">Add your restaurant</button>
-                <button className="flex">Sign up to deliver</button>
+
+            <div className=" pt-6">
+                <button className="flex pt-3">Create a business account</button>
+                <button className="flex pt-4">Add your restaurant</button>
+                <button className="flex pt-4">Sign up to deliver</button>
             </div>
         </div>
     )
