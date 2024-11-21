8c0caeee92636ff898acab2ba81e0602a55c9d1f
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-11-21 12:35:49 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { connectToMongoDB } from "@/lib/db";
 import { Header } from "../components/layout/Header";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <NuqsAdapter>{children}</NuqsAdapter>
       </body>
     </html>
   );
