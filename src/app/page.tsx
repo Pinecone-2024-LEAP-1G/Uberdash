0e55b2c499aa49aef4edd3bb951c9ad18c54222a
24LP4300 24LP4300 <24LP4300@admins-iMac.local>
2024-11-20 17:32:17 -0800

    restraunt-menu-item-finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,16 @@ const Home = () => {
         bonus="Buy 1,Get 1 Free"
       />
 
-      <RestrauntMenu />
+      <RestrauntMenu
+        name="Burrito Bowl"
+        price="$12.50"
+        percentage="80%"
+        like="(1100)"
+        discription=" Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa.Nulla consequat massa quis
+            enim."
+        image="https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvMzllMTJjODlhNzE2ZWEyYmYwNzE1MTM0MTBjYWE0Y2UvNTE0M2YxZTIxOGM2N2MyMGZlNWE0Y2QzM2Q5MGIwN2IuanBlZw=="
+      />
     </div>
   );
 
--- src/components/RestrauntMenu.tsx
+++ src/components/RestrauntMenu.tsx
@@ -1,31 +1,44 @@
 import { LikeSvg } from "../components/ui/Like-svg";
 import PlusSign from "../components/ui/PlusSignSvg";
 
-export const RestrauntMenu = () => {
+type Restraunt = {
+  name: string;
+  price: string;
+  percentage: string;
+  like: string;
+  discription: string;
+  image: string;
+};
+
+export const RestrauntMenu = ({
+  name,
+  price,
+  percentage,
+  like,
+  discription,
+  image,
+}: Restraunt) => {
   return (
     <div className="max-w-xl h-[147px] rounded-xl flex cursor-pointer justify-between border">
       <div className="w-[408px] flex py-4 pl-4 flex-col ">
-        <p className="text-[16px] font-medium">Burrito Bowl</p>
+        <p className="text-[16px] font-medium">{name}</p>
         <div className="flex items-center gap-1 text-[14px] font-normal mt-[4px]">
-          <p>$12.50 </p>
+          <p>{price}</p>
           <div className="w-[3px] h-[3px] bg-[black] rounded-full"></div>
           <LikeSvg />
-          <p>80%</p>
-          <p>(1100)</p>
+          <p>{percentage}</p>
+          <p>{like}</p>
         </div>
         <div className="mt-1">
           <p className=" text-ellipsis whitespace-nowrap  overflow-auto w-[408px] text-[#757575] text-[14px] font-normal">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa.Nulla consequat massa quis
-            enim.
+            {discription}
           </p>
         </div>
       </div>
       <div
         className="h-[145px] w-[145px] rounded-2xl relative "
         style={{
-          backgroundImage:
-            "url('https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvMzllMTJjODlhNzE2ZWEyYmYwNzE1MTM0MTBjYWE0Y2UvNTE0M2YxZTIxOGM2N2MyMGZlNWE0Y2QzM2Q5MGIwN2IuanBlZw==')",
+          backgroundImage: `url(${image})`,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
           backgroundSize: "cover",
