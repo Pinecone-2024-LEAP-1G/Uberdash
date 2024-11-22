1e4ee4e88a085de1d5c32d335a4a95a8b3553c34
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-11-22 10:06:25 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { Drawers } from "@/components/Drawer";
 import { MenuItem } from "../components/MenuItem";
 import { RestrauntMenu } from "../components/RestrauntMenu";
 import { MenuItemLastCard } from "../components/FoodCardPlus";
+import { PopOverTags } from "@/components/layout/PopOverTags";
 
 const Home = () => {
   return (
     <div>
       <FilterTags />
-      <Drawers />
+      <PopOverTags />
+      {/* <Drawers />
 
       <MenuItemLastCard
         image="https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvODExZDNlMzBjZDgxM2FjMzQ1ODZlMDUyMDA3ZDA0ODUvNTk1NGJjYjAwNmIxMGRiZmQwYmMxNjBmNjM3MGZhZjMuanBlZw=="
@@ -18,9 +20,9 @@ const Home = () => {
         price="$12.30"
         percentage="70%"
         like="(500)"
-      />
-      <Footer />
-    </div >
+      /> */}
+      {/* <Footer /> */}
+    </div>
   );
 };
 
