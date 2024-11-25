6468b8bbdfa6f620f2f8e3ae8a7987505b7b4829
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-11-25 10:36:53 +0800

    update

--- src/components/FastFoodFilter.tsx
+++ src/components/FastFoodFilter.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { MenuItem } from "./MenuItem";
+import axios from "axios";
+
 type MenuTypes = {
   image: string;
   name: string;
@@ -8,10 +11,31 @@ type MenuTypes = {
   points: string;
   bonus: string;
 };
-export const FastFoodFilter = ({image, name, duration, points, bonus} : MenuTypes) => {
-  return (
-    <div>
-      <MenuItem image={} name={} duration={} points={} bonus="{} />
-    </div>
-  );
+
+export const FastFoodFilter = ({
+  image,
+  name,
+  duration,
+  points,
+  bonus,
+}: MenuTypes) => {
+  const [mainFood, setMainFood] = useState<MenuTypes[]>([]);
+
+  const fetchdata = async () => {
+    try {
+      const response = await axios.get<{ foods: MenuTypes[] }>(
+        `${process.env.NEXT_PUBLIC_MONGODB_URI}/foods`
+      );
+
+      setMainFood(response.data.foods);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  return <div></div>;
 };
+//   <MenuItem image={} name={} duration={} points={} bonus={} />;
