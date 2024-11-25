19a907eb4361d9845f4b6058145e2e5a01eb4d7a
Ulemj Bayan-Undur <ue@Ue-MacBook-Pro.local>
2024-11-25 12:45:25 +0800

    restaurant review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,28 @@
-"use client";
-
 import FilterTags from "@/components/layout/FilterTags";
+import { Footer } from "@/components/Footer";
+import { Drawers } from "@/components/Drawer";
 import { MenuItem } from "../components/MenuItem";
 import { RestrauntMenu } from "../components/RestrauntMenu";
-import { MenuItemLastCard } from "../components/FoodCardPlus";
+import { MenuItemLastCard } from "../components";
+import { RestrauntReview } from "@/components/Restaurant-review";
 
 const Home = () => {
   return (
     <div>
-      <BasketDrawer />
-      <Link href="/owner/673e90415a6e8e222657bbb4">
-        <p> owner </p>
-      </Link>
-      <Drawers />
-      <div className="px-11">
-        <MenuItem
-          name=" Pappa Johns Burger"
-          image="https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvM2MzOTliMTJkYTliZDIzMjUyZmVhZjAyODk2MTBjYmMvNzgzNTQyOGIyODZhY2I1NzY0NmEyNTZjODk3YzBlOWUuanBlZw=="
-          duration=" 25-40 min"
-          points="4.3"
-          bonus="Buy 1,Get 1 Free"
-        />
+      <MenuItem
+        name=" Pappa Johns Burger"
+        image="https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvM2MzOTliMTJkYTliZDIzMjUyZmVhZjAyODk2MTBjYmMvNzgzNTQyOGIyODZhY2I1NzY0NmEyNTZjODk3YzBlOWUuanBlZw=="
+        duration=" 25-40 min"
+        points="4.3"
+        bonus="Buy 1,Get 1 Free"
+      />
 
-        <RestrauntMenu
-          name="Burrito Bowl"
-          price="$12.50"
-          percentage="80%"
-          like="(1100)"
-          discription=" Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+      <RestrauntMenu
+        name="Burrito Bowl"
+        price="$12.50"
+        percentage="80%"
+        like="(1100)"
+        discription=" Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
             commodo ligula eget dolor. Aenean massa.Nulla consequat massa quis
             enim."
         image="https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvMzllMTJjODlhNzE2ZWEyYmYwNzE1MTM0MTBjYWE0Y2UvNTE0M2YxZTIxOGM2N2MyMGZlNWE0Y2QzM2Q5MGIwN2IuanBlZw=="
@@ -44,7 +39,7 @@ const Home = () => {
         like="(500)"
       />
       <Footer />
-    </div >
+    </div>
   );
 };
 
