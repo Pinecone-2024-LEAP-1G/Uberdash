d4be0d0e6654c1c81dc925ae9f5308234bf13a46
24LP4300 24LP4300 <24LP4300@admins-iMac.local>
2024-11-26 18:46:59 -0800

    basket-drawer-duusav

--- src/components/basket-drawer/ThreeDotSelect.tsx
+++ src/components/basket-drawer/ThreeDotSelect.tsx
@@ -1,25 +1,57 @@
-"use client";
-
-import { TheaterIcon } from "lucide-react";
 import { useState } from "react";
+import ThreeDot from "../ui/ThreeDot";
+import PlusSign from "../ui/PlusSignSvg";
+import PersonPlus from "../ui/PersonPlus";
+import TrashIcon from "../ui/TrashIcon";
 
-export const ThreeDotModal = () => {
-  const [modal, setSmallModal] = useState(false);
-  const handleSmallModal = () => {
-    setSmallModal(!modal);
-  };
-
+export default function SmallModal() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => setIsOpen(!isOpen);
   return (
-    <div>
-      <div onClick={handleSmallModal}>
-        <TheaterIcon />
-      </div>
+    <div className="relative">
+      <button
+        onClick={toggleModal}
+        className="p-2 rounded-full hover:bg-gray-200"
+      >
+        <ThreeDot />
+      </button>
 
-      {modal && (
-        <div className="w-[309px] h-[192px]  px-4 py-3 justify-between items-center flex mr-12">
-          <p></p>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-[309px] h-[192px] rounded-xl shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <ul className="py-1">
+            <div className="flex items-center hover:bg-gray-100">
+              <div className="w-16 h-16 flex items-center justify-center">
+                <PlusSign />
+              </div>
+              <li>
+                <button className="block px-4 py-2 text-sm text-gray-700  w-full text-left">
+                  Add items
+                </button>
+              </li>
+            </div>
+            <div className="flex items-center hover:bg-gray-100 ">
+              <div className="w-16 h-16 flex items-center justify-center">
+                <PersonPlus />
+              </div>
+              <li>
+                <button className="block px-4 py-2 text-sm text-gray-700  w-full text-left">
+                  Group order
+                </button>
+              </li>
+            </div>
+            <div className="flex items-center hover:bg-red-50">
+              <div className="w-16 h-16 flex items-center justify-center">
+                <TrashIcon />
+              </div>
+              <li>
+                <button className="block px-4 py-2 text-sm text-red-600  w-full text-left">
+                  Clear Cart
+                </button>
+              </li>
+            </div>
+          </ul>
         </div>
       )}
     </div>
   );
-};
+}
--- src/components/ui/PlusSignSvg.tsx
+++ src/components/ui/PlusSignSvg.tsx
@@ -1,7 +1,7 @@
 const PlusSign = () => {
   return (
     <div>
-      <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
         <title>Plus small</title>
         <path
           d="M19 10.5h-5.5V5h-3v5.5H5v3h5.5V19h3v-5.5H19v-3Z"
