ead8cfbc84c0dd27cc2fe9ed081b7db49ac8de4e
24LP7314 24LP7314 <24LP7314@ADMINs-iMac.local>
2024-12-02 17:38:54 -0800

    implemneted menu category component

--- src/components/MenuCategory.tsx
+++ src/components/MenuCategory.tsx
@@ -1,16 +1,23 @@
-type menu = {
+type MenuCategoryProps = {
   title: string;
-  url: 'url("./https://cn-geo1.uber.com/static/mobile-content/eats/cuisine-filters/v1/Pizza.png")';
+  image: string;
 };
 
-const MenuCategory = (props: menu) => {
-  const { title } = props;
+export const MenuCategory = (props: MenuCategoryProps) => {
+  const { title, image } = props;
   return (
-    <div className="flex">
-      <img src={props.url} />
-      <p>Pizza</p>
+    <div className="flex flex-col items-center w-fit">
+      <img width={64} height={64} src={image} />
+      <p className="text-xs">{title}</p>
     </div>
   );
 };
 
-export default MenuCategory;
+{/* <MenuCategory title={"Pizza"} image={"https://cn-geo1.uber.com/static/mobile-content/eats/cuisine-filters/Grocery_v2.png"} /> */}
+
+
+
+
+
+
+
