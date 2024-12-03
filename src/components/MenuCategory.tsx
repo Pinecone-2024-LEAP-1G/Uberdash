135297e0b08786ab5a5978d7fe827837e3c732ba
24LP7314 24LP7314 <24LP7314@ADMINs-iMac.local>
2024-12-02 17:48:41 -0800

    implemented menu categories component

--- src/components/MenuCategory.tsx
+++ src/components/MenuCategory.tsx
@@ -6,18 +6,9 @@ type MenuCategoryProps = {
 export const MenuCategory = (props: MenuCategoryProps) => {
   const { title, image } = props;
   return (
-    <div className="flex flex-col items-center w-fit">
+    <div className="flex flex-col items-center w-fit cursor-pointer">
       <img width={64} height={64} src={image} />
       <p className="text-xs">{title}</p>
     </div>
   );
 };
-
-{/* <MenuCategory title={"Pizza"} image={"https://cn-geo1.uber.com/static/mobile-content/eats/cuisine-filters/Grocery_v2.png"} /> */}
-
-
-
-
-
-
-
