4454dad072c2aa9c1ee53ca7f5be937f1f838fbe
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-12-13 13:20:46 +0800

    update

---
+++ src/components/LottieLoader.tsx
@@ -0,0 +1,50 @@
+import { FC, memo, useRef } from "react";
+import Lottie from "react-lottie";
+
+interface ILottieAnimationProps {
+  animationData: unknown;
+  height?: string | number;
+  width?: string | number;
+  loop?: boolean;
+  autoplay?: boolean;
+  speed?: number;
+  className?: string;
+}
+
+const LottieAnimation: FC<ILottieAnimationProps> = memo(
+  ({
+    height = "auto",
+    width = "auto",
+    loop = true,
+    autoplay = true,
+    animationData,
+    className = `lottie__${crypto.randomUUID()}`,
+    speed = 1,
+  }) => {
+    const ref = useRef(null);
+
+    const options = {
+      loop,
+      autoplay,
+      animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+        className,
+      },
+    };
+
+    return (
+      <Lottie
+        options={options}
+        height={height}
+        width={width}
+        speed={speed}
+        ref={ref}
+      />
+    );
+  }
+);
+
+LottieAnimation.displayName = "LottieAnimation";
+
+export default LottieAnimation;
