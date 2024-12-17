6453dd14b76502b066deafdaf79f7f5fd733bf81
24LP9404 24LP9404 <24LP9404@admins-iMac.local>
2024-12-17 12:08:20 +0800

    fb, insta, twitter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,9 @@ export const Footer: React.FC<FooterProps> = ({ className = "" }) => {
     <footer className={`py-12 mb-3 mt-40 ${className}`}>
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-8 px-6">
         <div className="flex flex-col items-center md:items-start">
-          <p className="text-3xl font-extrabold text-gray-800">Хурдан хоол</p>
+          <Link href="/">
+            <p className="text-3xl font-extrabold text-gray-800">Хурдан хоол</p>
+          </Link>
           <p className="text-sm text-gray-600 mt-2 text-center md:text-left">
             Таны дуртай ресторанууд, түргэн хүргэлттэйгээр
           </p>
@@ -38,24 +40,33 @@ export const Footer: React.FC<FooterProps> = ({ className = "" }) => {
         </div>
 
         <div className="flex gap-4">
-          <button
-            aria-label="Facebook"
-            className="p-3 bg-white rounded-full shadow-lg hover:shadow-xl hover:bg-blue-100 transition-all"
+          <Link
+            href="https://www.facebook.com/profile.php?id=100074642433564"
+            target="_blank"
           >
-            <Facebook size={20} className="text-blue-600" />
-          </button>
-          <button
-            aria-label="Twitter"
-            className="p-3 bg-white rounded-full shadow-lg hover:shadow-xl hover:bg-blue-100 transition-all"
-          >
-            <Twitter size={20} className="text-blue-400" />
-          </button>
-          <button
-            aria-label="Instagram"
-            className="p-3 bg-white rounded-full shadow-lg hover:shadow-xl hover:bg-pink-100 transition-all"
-          >
-            <Instagram size={20} className="text-pink-500" />
-          </button>
+            <button
+              aria-label="Facebook"
+              className="p-3 bg-white rounded-full shadow-lg hover:shadow-xl hover:bg-blue-100 transition-all"
+            >
+              <Facebook size={20} className="text-blue-600" />
+            </button>
+          </Link>
+          <Link href="https://x.com/?lang=en" target="_blank">
+            <button
+              aria-label="Twitter"
+              className="p-3 bg-white rounded-full shadow-lg hover:shadow-xl hover:bg-blue-100 transition-all"
+            >
+              <Twitter size={20} className="text-blue-400" />
+            </button>
+          </Link>
+          <Link href="https://www.instagram.com/" target="_blank">
+            <button
+              aria-label="Instagram"
+              className="p-3 bg-white rounded-full shadow-lg hover:shadow-xl hover:bg-pink-100 transition-all"
+            >
+              <Instagram size={20} className="text-pink-500" />
+            </button>
+          </Link>
         </div>
       </div>
 
