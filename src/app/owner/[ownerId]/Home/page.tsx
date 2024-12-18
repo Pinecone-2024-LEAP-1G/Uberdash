d389a3e307ffde23942bff21a4d10bde7f757873
24LP8458 24LP8458 <24LP8458@admins-iMac.local>
2024-12-18 11:50:10 +0800

    updated design

--- src/app/owner/[ownerId]/Home/page.tsx
+++ src/app/owner/[ownerId]/Home/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { AdminSideBar } from "@/components/AdminSideBoard";
 import { AdminBarChart } from "@/components/BarChart";
 import { PieChartByCategory } from "@/components/PieChartByCategory";
 
 const Info = () => {
   return (
-    <div className="p-4 flex  w-full">
-      <div>
+    <div className="p-4 flex w-full ml-72">
+      <div className="flex flex-col gap-6 w-full max-w-4xl mx-auto">
         <AdminBarChart />
-      </div>
-      <div>
         <PieChartByCategory />
       </div>
     </div>
   );
 };
+
 export default Info;
